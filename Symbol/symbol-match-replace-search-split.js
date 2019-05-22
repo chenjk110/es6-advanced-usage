04fdf8947d4ecb9c9c1fc41c9289d0efcae18933
chenjk110 <chenjk110@qq.com>
2019-05-22 14:57:33 +0800

    add Symbol.match

---
+++ Symbol/symbol-match-replace-search-split.js
@@ -0,0 +1,50 @@
+/**
+ * Symbol.match
+ */
+
+// 原始使用
+console.log('abc'.match(/a/)) // [ 'a', index: 0, input: 'abc' 
+
+
+/**
+ * String.prototype.match()函数可传入
+ * 1. { [Symbol.match]: (value: string): RegExpMatchArray }
+ * 2. RegExp | string
+ */
+
+
+// 构造自定义match规则对象
+const isLenLeast10 = {
+  [Symbol.match]: function (value) {
+    return value.length >= 10 ? [value.substr(0, 10)] : null
+  }
+}
+
+const testStr1 = 'a'.repeat(10)
+const testStr2 = 'b'.repeat(9)
+
+console.log(
+  testStr1.match(isLenLeast10), // ['aaaaaaaaaa']
+  testStr2.match(isLenLeast10), // null
+)
+
+// 规则：匹配查找'a'字符，匹配出包含'a'字符后面最多三位字符串
+const testStr3 = 'apple orange'
+const find3LetterOfA = {
+  [Symbol.match]: function (value) {
+    const res = []
+    let idx = 0
+    if (!value.length) return ''
+    while(idx < value.length) {
+      if (value[idx] === 'a') {
+        res.push(value.substr(idx, 3))
+      }
+      idx++
+    }
+    return res.length ? res : null
+  }
+}
+
+console.log(testStr3.match(find3LetterOfA)) // ['app', 'ang']
+
+
